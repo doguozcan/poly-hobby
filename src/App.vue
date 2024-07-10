f51f9a7d821768322478da9af44308f7dab0b3a1
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-10 12:38:56 +0300

    Implement a list and display it using v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+// hobbies array
+const hobbies = ref([
+  { category: 'Computer Science', activity: 'Code a webpage with vue.' },
+  { category: 'Basketball', activity: 'Do a shooting training.' },
+  { category: 'Movies', activity: 'Watch Rocky.' },
+  { category: 'Basketball', activity: 'Watch an NBA game.' },
+  { category: 'Computer Science', activity: 'Train competitive programming.' },
+])
+</script>
 
 <template>
   <h1>Poly Hobby</h1>
-  <p>Welcome to the polly hobby</p>
+  <ul>
+    <li v-for="hobby in hobbies">
+      {{ hobby.category }} - {{ hobby.activity }}
+    </li>
+  </ul>
 </template>
 
 <style></style>
